2a6dd2816240866403d4a391fb3a183aacd15c92
vboxuser <vboxuser@Ubunto.myguest.virtualbox.org>
2024-09-03 16:33:05 +0200

    Add code for TB6600

--- Software/TestMotor/src/main.cpp
+++ Software/TestMotor/src/main.cpp
@@ -1,9 +1,14 @@
 #include "main.h"
 
+// Definește pinii pentru TB6600
+#define DIR_PIN 2
+#define STEP_PIN 3
+#define ENABLE_PIN 4 // Dacă folosești pinul de activare
+
 // Motor constants
-uint16_t motorSpeed = 1;
-uint16_t motorDelay = 5;           // 0.01 seconds
-uint8_t motorDirection = BACKWARD; // Direcția motorului (FORWARD sau BACKWARD)
+uint16_t motorSpeed = 1; // Viteza motorului (pași pe secundă)
+uint16_t motorDelay = 1000 / motorSpeed; // Întârzierea între pași în milisecunde
+uint8_t motorDirection = LOW; // Direcția motorului (LOW sau HIGH)
 
 // Time constants
 uint32_t curMotorTime = 0;
@@ -11,10 +16,8 @@ uint32_t curPrintTime = 0;
 uint32_t curCommunicationTime = 0;
 uint32_t curEncoderTime = 0;
 
-AF_Stepper motor(STEPS, STEPPERNR); // uint16_t steps - revolution per steps, uint8_t num connected to shield
-
+// Encoder și LCD
 Encoder enc(CLK, DT, SW);
-
 LiquidCrystal_I2C lcd(ADDRESS, 16, 2);
 
 struct Flag
@@ -29,16 +32,19 @@ int voltageValue(uint16_t adcValue)
 
 void setup()
 {
-  // close relay
-  digitalWrite(RELAY, LOW);
-  digitalWrite(RELAY1, LOW);
+  pinMode(DIR_PIN, OUTPUT);
+  pinMode(STEP_PIN, OUTPUT);
+  pinMode(ENABLE_PIN, OUTPUT);
+
+  // Dezactivează motorul la început
+  digitalWrite(ENABLE_PIN, LOW);
 
   Serial.begin(115200);
-  delay(100); // Give time for Serial to initialize
+  delay(100);
 
   Serial.println("System Initialized\r\n");
   Serial.println("Available commands:\r\n");
-  Serial.println("m <value> - Set motor speed (RPM)\r\n");
+  Serial.println("m <value> - Set motor speed (steps per second)\r\n");
   Serial.println("s <0/1> - Stop/Start motor\r\n");
   Serial.println("d <0/1> - Set motor direction (0 = BACKWARD, 1 = FORWARD)\r\n");
 
@@ -47,18 +53,18 @@ void setup()
   curCommunicationTime = millis();
   curEncoderTime = millis();
 
-  // Encoder type one spet ore 2
+  // Encoder type one step or 2
   enc.setType(TYPE1);
 
   // LCD
   lcd.init();
-  lcd.backlight(); // Включаем подсветку дисплея
+  lcd.backlight();
   lcd.setCursor(0, 0);
   lcd.print("V:" + String(int(voltageValue(analogRead(ADC_PIN)) / 1000.0)));
   lcd.setCursor(5, 0);
   lcd.print("kV RPM:" + String(motorSpeed));
   lcd.setCursor(0, 1);
-  if (motorDirection == FORWARD)
+  if (motorDirection == HIGH)
     lcd.print("d:F");
   else
     lcd.print("d:B");
@@ -68,7 +74,6 @@ void setup()
 
 void loop()
 {
-  // encoder
   enc.tick();
 
   if (millis() - curEncoderTime >= ENCODER_DELAY)
@@ -77,88 +82,59 @@ void loop()
 
     if (enc.isClick())
     {
-      if (flag.start)
-      {
-        flag.start = 0;
-        digitalWrite(RELAY, LOW);
-        digitalWrite(RELAY1, LOW);
-      }
-
-      else
-      {
-        flag.start = 1;
-        digitalWrite(RELAY, HIGH);
-        digitalWrite(RELAY1, HIGH);
-      }
-
-      Serial.println("Encoder cklick start, value: " + String(flag.start));
+      flag.start = !flag.start;
+      digitalWrite(ENABLE_PIN, flag.start ? HIGH : LOW);
+      Serial.println("Encoder click start, value: " + String(flag.start));
       lcd.setCursor(5, 1);
       lcd.print("s:" + String(flag.start));
     }
     else if (enc.isDouble())
     {
-      if (motorDirection == FORWARD)
-      {
-        motorDirection = BACKWARD;
-        Serial.println("Encoder cklick direction, value: BACKWARD ");
-
-        lcd.setCursor(0, 1);
-        lcd.print("d:B");
-      }
-      else
-      {
-        motorDirection = FORWARD;
-        Serial.println("Encoder cklick direction, value: FORWARD ");
-
-        lcd.setCursor(0, 1);
-        lcd.print("d:F");
-      }
+      motorDirection = !motorDirection;
+      digitalWrite(DIR_PIN, motorDirection);
+      Serial.println("Encoder click direction, value: " + String(motorDirection ? "FORWARD" : "BACKWARD"));
+      lcd.setCursor(0, 1);
+      lcd.print(motorDirection ? "d:F" : "d:B");
     }
     else if (enc.isRight())
     {
       motorSpeed++;
       if (motorSpeed > 100)
         motorSpeed = 100;
-      Serial.println("Encoder cklick speed, value: " + String(motorSpeed));
+      motorDelay = 1000 / motorSpeed;
+      Serial.println("Encoder click speed, value: " + String(motorSpeed));
       lcd.setCursor(5, 0);
       lcd.print("kV RPM:" + String(motorSpeed));
     }
     else if (enc.isLeft())
     {
       motorSpeed--;
-      if (motorSpeed == 0)
+      if (motorSpeed < 1)
         motorSpeed = 1;
-      Serial.println("Encoder cklick speed, value: " + String(motorSpeed));
+      motorDelay = 1000 / motorSpeed;
+      Serial.println("Encoder click speed, value: " + String(motorSpeed));
       lcd.setCursor(5, 0);
       lcd.print("kV RPM:" + String(motorSpeed));
     }
   }
 
-  // Example to rotate the motor in one direction with a specified speed
-  if (flag.start)
+  if (flag.start && millis() - curMotorTime >= motorDelay)
   {
-    // if (millis() - curMotorTime >= motorDelay)
-    // {
-    // curMotorTime = millis();
-
-    // Set initial motor direction and speed
-    motor.setSpeed(motorSpeed); // Set motor speed in RPM
-    motor.release();            // Ensure motor is released at start
-
-    motor.step(1, motorDirection, SINGLE);
-    // }
+    curMotorTime = millis();
+    digitalWrite(STEP_PIN, HIGH);
+    delayMicroseconds(100);
+    digitalWrite(STEP_PIN, LOW);
+    delayMicroseconds(100);
   }
 
   if (millis() - curPrintTime >= PRINT_DELAY)
   {
     curPrintTime = millis();
 
-    // print moror speed
     Serial.print("Motor Speed: ");
     Serial.print(motorSpeed);
-    Serial.println(" RPM");
+    Serial.println(" steps/sec");
 
-    // print voltage
     Serial.print("Voltage: ");
     Serial.println(voltageValue(analogRead(ADC_PIN)));
     lcd.setCursor(0, 0);
@@ -169,8 +145,7 @@ void loop()
   {
     curCommunicationTime = millis();
 
-    // Read command from Windows app
-    if (Serial.available() > true)
+    if (Serial.available() > 0)
     {
       char key = Serial.read();
       int val = Serial.parseInt();
@@ -179,38 +154,20 @@ void loop()
       {
       case 's':
         flag.start = val;
-        if (flag.start == 0)
-        {
-          digitalWrite(RELAY, LOW);
-          digitalWrite(RELAY1, LOW);
-        }
-        else
-        {
-          digitalWrite(RELAY, HIGH);
-          digitalWrite(RELAY1, HIGH);
-        }
-
+        digitalWrite(ENABLE_PIN, flag.start ? HIGH : LOW);
         Serial.println("command: s, value: " + String(flag.start));
         break;
       case 'd':
-        if (val == 0)
-        {
-          motorDirection = BACKWARD;
-          Serial.println("command: d, value: BACKWARD ");
-        }
-        else
-        {
-          motorDirection = FORWARD;
-          Serial.println("command: d, value: FORWARD ");
-        }
+        motorDirection = val ? HIGH : LOW;
+        digitalWrite(DIR_PIN, motorDirection);
+        Serial.println("command: d, value: " + String(motorDirection ? "FORWARD" : "BACKWARD"));
         break;
       case 'm':
         motorSpeed = val;
+        motorDelay = 1000 / motorSpeed;
         Serial.println("command: m, value: " + String(motorSpeed));
         break;
-
       default:
-        // Comanda gresita
         Serial.println("Wrong command! key: " + String(key) + ", val: " + String(val));
         break;
       }
