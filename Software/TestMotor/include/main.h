7b3738425a71fb0b314e2e7657c4cb26f3a144bb
Creciunel <60750488+Creciunel@users.noreply.github.com>
2024-10-08 18:39:29 +0300

    FreeRTOS

--- Software/TestMotor/include/main.h
+++ Software/TestMotor/include/main.h
@@ -1,47 +1,50 @@
-#include <Wire.h> 
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-
 #include <Arduino.h>
-#include <AFMotor.h>
 #include <GyverEncoder.h>
 
-// #include <Stepper.h>
+#include <Arduino_FreeRTOS.h>
 
-#define ADDRESS 0x27 // I2C address
+#define ADDRESS           0x27 // I2C address
+#define SCL_PIN          21
+#define SDA_PIN          20
 
-// Stepper motor constants
-#define STEPPERNR 1
-#define STEPS 200
-#define COIL_A 8
-#define COIL_B 9
-#define COIL_C 10
-#define COIL_D 11
-
-// #define FORWARD 1
-// #define BACKWARD 0
-
+// Definește pinii pentru TB6600
+#define DIR_PIN          44   // DIR+
+#define PUL_PIN          46   // PUL+
+#define ENA_PIN          48   // ENA+
 
 // ADC constants
-#define ADC_PIN A8 // ADC pin
-#define ADC_RESOLUTION 5.0 / 1024.0
-#define R2 10151.5 // 10k
-#define R1 96840000 // 100k
-// Calculate voltage gain between 2 resistors
+#define ADC_PIN          A8 // ADC pin
+#define ADC_RESOLUTION    5.0 / 1024.0
+#define R2                10151.5 // R2 resistor value in ohms
+#define R1                96840000 // R1 resistor value in ohms
+
+// Calculate voltage gain between two resistors
 #define VOLTAGE_GAIN ((R1 + R2) / R2)
 
 // Time constants
-#define PRINT_DELAY 2000
-#define COMMUNICATION_DELAY 100
-#define ENCODER_DELAY 100
+#define PRINT_DELAY      2000
+#define COMMUNICATION_DELAY   100
+#define ENCODER_DELAY      100
 
 // Helper functions
 #define MillisecondsToMinutes(milliseconds) ((milliseconds) / 60000.0)
 
 // Encoder constants
-#define  CLK 26
-#define  DT  24
-#define  SW  22
+#define CLK              26
+#define DT                24
+#define SW                22
 
 // Relay constants
-#define  RELAY 30
-#define  RELAY1 32
+#define RELAY             30
+#define RELAY1            32
+
+#define PWM_PIN          13 
+
+int voltageValue(uint16_t adcValue)
+{
+  return (ADC_RESOLUTION * adcValue) * VOLTAGE_GAIN;
+}
+
+void checkEncoder();
--- Software/TestMotor/src/main.cpp
+++ Software/TestMotor/src/main.cpp
@@ -1,23 +1,21 @@
 #include "main.h"
 
-// Definește pinii pentru TB6600
-#define DIR_PIN 2
-#define STEP_PIN 3
-#define ENABLE_PIN 4 // Dacă folosești pinul de activare
+void vTaskEncoder(void *pvParameters);
+void vTaskMotor(void *pvParameters);
+void vTaskLCD(void *pvParameters);
+void vTaskCommunication(void *pvParameters);
+void vTaskTransformer(void *pvParameters);
 
 // Motor constants
-uint16_t motorSpeed = 1; // Viteza motorului (pași pe secundă)
-uint16_t motorDelay = 1000 / motorSpeed; // Întârzierea între pași în milisecunde
-uint8_t motorDirection = LOW; // Direcția motorului (LOW sau HIGH)
+uint16_t motorSpeed = 1;
+uint16_t motorDelay = 1000 / motorSpeed;
+uint8_t motorDirection = LOW;
 
-// Time constants
-uint32_t curMotorTime = 0;
-uint32_t curPrintTime = 0;
-uint32_t curCommunicationTime = 0;
-uint32_t curEncoderTime = 0;
+// Transformer variables
+uint8_t pwmValue = 0;
 
 // Encoder și LCD
-Encoder enc(CLK, DT, SW);
+Encoder enc(CLK, DT, SW, TYPE1);
 LiquidCrystal_I2C lcd(ADDRESS, 16, 2);
 
 struct Flag
@@ -25,152 +23,204 @@ struct Flag
   uint8_t start = 0;
 } flag;
 
-int voltageValue(uint16_t adcValue)
-{
-  return (ADC_RESOLUTION * adcValue) * VOLTAGE_GAIN;
-}
-
 void setup()
 {
-  pinMode(DIR_PIN, OUTPUT);
-  pinMode(STEP_PIN, OUTPUT);
-  pinMode(ENABLE_PIN, OUTPUT);
+  // Crearea sarcinilor
+  xTaskCreate(vTaskEncoder, "Encoder Task", 128, NULL, 2, NULL);
+  xTaskCreate(vTaskMotor, "Motor Task", 128, NULL, 1, NULL);
+  xTaskCreate(vTaskLCD, "LCD Task", 128, NULL, 1, NULL);
+  xTaskCreate(vTaskCommunication, "Communication Task", 128, NULL, 1, NULL);
+  xTaskCreate(vTaskTransformer, "Transformer Task", 128, NULL, 1, NULL);
 
-  // Dezactivează motorul la început
-  digitalWrite(ENABLE_PIN, LOW);
+  // Pornește scheduler-ul FreeRTOS
+  vTaskStartScheduler();
 
+// Initializarea LCD
+  lcd.init(); // Asigură-te că LCD-ul este inițializat
+  lcd.backlight(); // Activează iluminarea de fundal
+
+  // initializarea seriala
   Serial.begin(115200);
-  delay(100);
+}
 
-  Serial.println("System Initialized\r\n");
-  Serial.println("Available commands:\r\n");
-  Serial.println("m <value> - Set motor speed (steps per second)\r\n");
-  Serial.println("s <0/1> - Stop/Start motor\r\n");
-  Serial.println("d <0/1> - Set motor direction (0 = BACKWARD, 1 = FORWARD)\r\n");
+void loop() {}
 
-  curPrintTime = millis();
-  curMotorTime = millis();
-  curCommunicationTime = millis();
-  curEncoderTime = millis();
+void vTaskEncoder(void *pvParameters)
+{
+  pinMode(SW, INPUT_PULLUP);
+  pinMode(CLK, INPUT_PULLUP);
+  pinMode(DT, INPUT_PULLUP);
 
-  // Encoder type one step or 2
-  enc.setType(TYPE1);
+  for (;;)
+  {
+    enc.tick(); // Actualizează starea encoderului
 
-  // LCD
+    checkEncoder();
+
+    vTaskDelay(100 / portTICK_PERIOD_MS); // Întârziere mai mică pentru reacții rapide
+  }
+}
+
+void vTaskMotor(void *pvParameters)
+{
+  pinMode(DIR_PIN, OUTPUT);
+  pinMode(PUL_PIN, OUTPUT);
+  pinMode(ENA_PIN, OUTPUT);
+
+  digitalWrite(ENA_PIN, LOW);
+  digitalWrite(DIR_PIN, HIGH);
+
+  for (;;)
+  {
+    if (flag.start)
+    {
+      digitalWrite(PUL_PIN, HIGH);
+      delayMicroseconds(100);
+      digitalWrite(PUL_PIN, LOW);
+      delayMicroseconds(100);
+    }
+    vTaskDelay(10 / portTICK_PERIOD_MS); // Delay mai mic pentru reacții rapide
+  }
+}
+
+void vTaskLCD(void *pvParameters)
+{
+  
   lcd.init();
   lcd.backlight();
+  
   lcd.setCursor(0, 0);
   lcd.print("V:" + String(int(voltageValue(analogRead(ADC_PIN)) / 1000.0)));
   lcd.setCursor(5, 0);
   lcd.print("kV RPM:" + String(motorSpeed));
   lcd.setCursor(0, 1);
+
   if (motorDirection == HIGH)
     lcd.print("d:F");
   else
     lcd.print("d:B");
+
   lcd.setCursor(5, 1);
   lcd.print("s:" + String(flag.start));
+
+  for (;;)
+  {
+    lcd.setCursor(0, 0);
+    lcd.print("V:" + String(int(voltageValue(analogRead(ADC_PIN)) / 1000.0)));
+
+    vTaskDelay(1000 / portTICK_PERIOD_MS); // Actualizează LCD-ul la fiecare secundă
+  }
 }
 
-void loop()
+void vTaskTransformer(void *pvParameters)
 {
-  enc.tick();
+  // init transformer pin
+  pinMode(PWM_PIN, OUTPUT);
 
-  if (millis() - curEncoderTime >= ENCODER_DELAY)
+  // Setare Timer0 pentru a obține o frecvență de aproximativ 15.625 kHz
+  TCCR0A = (1 << WGM00) | (1 << WGM01);
+  TCCR0B = (1 << CS01);
+
+  for (;;)
   {
-    curEncoderTime = millis();
+    analogWrite(PWM_PIN, pwmValue);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+
+void vTaskCommunication(void *pvParameters)
+{
+  Serial.println("System Initialized\r\n");
+  Serial.println("Available commands:\r\n");
+  Serial.println("m <value> - Set motor speed (steps per second)\r\n");
+  Serial.println("s <0/1> - Stop/Start motor\r\n");
+  Serial.println("d <0/1> - Set motor direction (0 = BACKWARD, 1 = FORWARD)\r\n");
+  Serial.println("p <0/255> - Set PWM value for Transformer\r\n");
+
+  for (;;)
+  {
+    if (Serial.available() > 0)
+    {
+      char key = Serial.read();
+      int val = Serial.parseInt();
+      
+
+      switch (key)
+      {
+      case 's':
+        flag.start = val;
+        digitalWrite(ENA_PIN, flag.start ? LOW : HIGH);
+        break;
+      case 'd':
+        motorDirection = val ? HIGH : LOW;
+        digitalWrite(DIR_PIN, motorDirection);
+        break;
+      case 'm':
+        motorSpeed = val;
+        motorDelay = max(1000 / motorSpeed, 1);
+        break;
+      case 'p':
+        pwmValue = val;
+
+        if (pwmValue > 255)
+          pwmValue = 255;
+        else if (pwmValue < 0)
+          pwmValue = 0;
+
+        Serial.println("command: p, value: " + String(pwmValue));
+        break;
+      default:
+        Serial.println("Wrong command! key: " + String(key) + ", val: " + String(val));
+        break;
+      }
+    }
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+  }
+}
+
+void checkEncoder()
+{
+  static uint32_t lastCheckTime = 0;
+
+  if (millis() - lastCheckTime >= ENCODER_DELAY)
+  {
+    lastCheckTime = millis();
 
     if (enc.isClick())
     {
       flag.start = !flag.start;
-      digitalWrite(ENABLE_PIN, flag.start ? HIGH : LOW);
-      Serial.println("Encoder click start, value: " + String(flag.start));
+      digitalWrite(ENA_PIN, flag.start ? LOW : HIGH);
       lcd.setCursor(5, 1);
       lcd.print("s:" + String(flag.start));
+      Serial.println("command: s, value: " + String(flag.start));
     }
     else if (enc.isDouble())
     {
       motorDirection = !motorDirection;
       digitalWrite(DIR_PIN, motorDirection);
-      Serial.println("Encoder click direction, value: " + String(motorDirection ? "FORWARD" : "BACKWARD"));
       lcd.setCursor(0, 1);
       lcd.print(motorDirection ? "d:F" : "d:B");
+      Serial.println("command: d, value: " + String(motorDirection));
     }
     else if (enc.isRight())
     {
       motorSpeed++;
       if (motorSpeed > 100)
         motorSpeed = 100;
-      motorDelay = 1000 / motorSpeed;
-      Serial.println("Encoder click speed, value: " + String(motorSpeed));
+      motorDelay = max(1000 / motorSpeed, 1);
       lcd.setCursor(5, 0);
       lcd.print("kV RPM:" + String(motorSpeed));
+      Serial.println("command: m, value: " + String(motorSpeed));
     }
     else if (enc.isLeft())
     {
       motorSpeed--;
       if (motorSpeed < 1)
         motorSpeed = 1;
-      motorDelay = 1000 / motorSpeed;
-      Serial.println("Encoder click speed, value: " + String(motorSpeed));
+      motorDelay = max(1000 / motorSpeed, 1);
       lcd.setCursor(5, 0);
       lcd.print("kV RPM:" + String(motorSpeed));
-    }
-  }
-
-  if (flag.start && millis() - curMotorTime >= motorDelay)
-  {
-    curMotorTime = millis();
-    digitalWrite(STEP_PIN, HIGH);
-    delayMicroseconds(100);
-    digitalWrite(STEP_PIN, LOW);
-    delayMicroseconds(100);
-  }
-
-  if (millis() - curPrintTime >= PRINT_DELAY)
-  {
-    curPrintTime = millis();
-
-    Serial.print("Motor Speed: ");
-    Serial.print(motorSpeed);
-    Serial.println(" steps/sec");
-
-    Serial.print("Voltage: ");
-    Serial.println(voltageValue(analogRead(ADC_PIN)));
-    lcd.setCursor(0, 0);
-    lcd.print("V:" + String(int(voltageValue(analogRead(ADC_PIN)) / 1000.0)));
-  }
-
-  if (millis() - curCommunicationTime >= COMMUNICATION_DELAY)
-  {
-    curCommunicationTime = millis();
-
-    if (Serial.available() > 0)
-    {
-      char key = Serial.read();
-      int val = Serial.parseInt();
-
-      switch (key)
-      {
-      case 's':
-        flag.start = val;
-        digitalWrite(ENABLE_PIN, flag.start ? HIGH : LOW);
-        Serial.println("command: s, value: " + String(flag.start));
-        break;
-      case 'd':
-        motorDirection = val ? HIGH : LOW;
-        digitalWrite(DIR_PIN, motorDirection);
-        Serial.println("command: d, value: " + String(motorDirection ? "FORWARD" : "BACKWARD"));
-        break;
-      case 'm':
-        motorSpeed = val;
-        motorDelay = 1000 / motorSpeed;
-        Serial.println("command: m, value: " + String(motorSpeed));
-        break;
-      default:
-        Serial.println("Wrong command! key: " + String(key) + ", val: " + String(val));
-        break;
-      }
+      Serial.println("command: m, value: " + String(motorSpeed));
     }
   }
 }
